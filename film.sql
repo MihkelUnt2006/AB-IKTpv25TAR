ff8d36c33348967d79ef7bb901c9278a98b3ee6b
Mihkel <mihunt29@gmail.com>
2026-02-05 16:54:07 +0200

    Add genre and producer tables with relationships
    Created tables for genres and producers, added foreign keys, and inserted initial data.

--- film.sql
+++ film.sql
@@ -29,3 +29,43 @@ SELECT * FROM rezisoor;
 
 Insert into film( filmNimetus, rezisoorID, pikkus, v_aasta)
 Values('rapuntsel', 4, 120, 2021) 
+--table zanr
+CREATE TABLE zanr(
+zanrID int PRIMARY KEY identity (1,1),
+zanrNimetus varchar(50) UNIQUE,
+kirjeldus TEXT)
+
+INSERT INTO zanr(zanrNimetus, kirjeldus)
+VALUES ('komöödia', 'on põnev film ')
+SELECT * FROM zanr; 
+--tabeli struktuuri muutimine 
+ALTER TABLE film ADD zanrID int 
+select * from film 
+--FK lisamine 
+ALTER TABLE film ADD CONSTRAINT fk_zanr
+FOREIGN KEY (zanrID) references zanr(zanrID);
+--tabeli uuendamine 
+UPDATE film SET zanrID=2 WHERE filmID=2;
+UPDATE film SET zanrID=1 WHERE filmID>=3;
+
+CREATE TABLE tootja(
+tootjaID int PRIMARY KEY identity (1,1),
+tootjaNimi varchar(50) UNIQUE,
+asukoht TEXT)
+select * from tootja
+INSERT INTO tootja(tootjaNimi, asukoht)
+VALUES ('disney', 'Berlin'),
+('Universal','Hollywood')
+
+CREATE TABLE tootjaFilm(
+tootjafilm int PRIMARY KEY identity (1,1),
+tootjaID int, 
+FOREIGN KEY (tootjaID) REFERENCES tootja(tootjaID),
+FilmID int,
+FOREIGN KEY (FilmID) REFERENCES film(filmID)
+);
+Select * from tootjaFilm
+Select * from tootja
+Select * from film;
+INSERT INTO tootjaFilm (tootjaID, FilmID)
+Values (2 ,3)
